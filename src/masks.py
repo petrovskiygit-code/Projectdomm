2383d92b10c9467021aa7e987ae3f103c6f4047e
petrovskiygit-code <petrovskiygit@gmail.com>
2025-12-02 14:03:10 +0300

    init

---
+++ src/masks.py
@@ -0,0 +1,8 @@
+def get_mask_card_number(number: str) -> str:
+    """Маскирует номер карты"""
+    return f"{number[:4]} {number[4:6]}** **** {number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Маскирует номер акаунта"""
+    return f"**{account_number[-4:]}"
